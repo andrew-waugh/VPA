f749930cde848b0a51a6a9f2068fa6dc84c1f45f
Vic4mz9 <Vic4mz9@WDAUD70209LW.internal.vic.gov.au>
2019-11-22 16:48:42 +1100

    Cleaned up bin directories to make it more portable

--- VPA.bat
+++ VPA.bat
@@ -1,7 +1,5 @@
 @echo off
-set code="C:\Users\Andrew\Documents\Work\VERSCode\VPA"
-set bin="C:\Program Files\Java\jdk1.8.0_162\bin"
-rem set code="J:\PROV\TECHNOLOGY MANAGEMENT\Application Development\VERS\VERS-2015\VPA"
-rem set bin="C:\Program Files\Java\jdk1.8.0_144\bin"
+rem set code="C:\Users\Andrew\Documents\Work\VERSCode\VPA"
+set code="J:\PROV\TECHNOLOGY MANAGEMENT\Application Development\VERS\VERSCode\VPA"
 set versclasspath=%code%/dist/*
-%bin%\java -classpath %versclasspath% VPA.DAIngest -v -s "%code%/support" %*
+java -classpath %versclasspath% VPA.DAIngest -s %code%/support %*
