4176ab9ea44043880534b2129e357389e7fd613a
Andrew Waugh <andrew.waugh@prov.vic.gov.au>
2022-03-30 17:43:23 +1100

    Minor corrections to BAT files to handle VERS code in C:/Program Files.

--- VPA.bat
+++ VPA.bat
@@ -1,6 +1,8 @@
 @echo off
 if exist "J:/PROV/TECHNOLOGY MANAGEMENT/Application Development/VERS/VERSCode" (
 	set code="J:/PROV/TECHNOLOGY MANAGEMENT/Application Development/VERS/VERSCode"
+) else if exist "C:/Program Files/VERSCode" (
+	set code="C:/Program Files/VERSCode"
 ) else if exist "Z:/VERSCode" (
 	set code="Z:/VERSCode"
 ) else if exist "C:/Program Files/VERSCode" (
