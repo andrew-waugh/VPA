95027fdd519908e65460303c7d650e641fef19ce
Andrew Waugh <andrew.waugh@gmail.com>
2025-03-03 12:10:59 +1100

    Updated VPA.bat and deleted the support directory. Now use the support directory in VERSCommon

--- VPA.bat
+++ VPA.bat
@@ -5,7 +5,9 @@ if exist "J:/PROV/TECHNOLOGY MANAGEMENT/Application Development/VERS/VERSCode" (
 	set code="C:/Program Files/VERSCode"
 ) else if exist "Z:/VERSCode" (
 	set code="Z:/VERSCode"
+) else if exist "C:/Program Files/VERSCode" (
+	set code="C:/Program Files/VERSCode"
 ) else (
-	set code="C:/Users/Andrew/Documents/Work/VERSCode"
+	set code="C:/Users/andre/Documents/Work/VERSCode"
 )
-java -classpath %code%/VPA/dist/* VPA.DAIngest -s %code%/VPA/support %*
+java -Djava.util.logging.config.file=%code%/VERSCommon/VERSSupportFiles/vpalog.properties -classpath %code%/VPA/dist/* VPA.DAIngest -s %code%/VERSCommon/VERSSupportFiles -dasmode %*
